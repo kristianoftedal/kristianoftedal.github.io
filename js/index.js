9bedc06c8cf6c55b4264ac8422b3e6ae80cb8653
Kristian Oftedal <kristian.oftedal@pointtaken.no>
2016-09-05 15:07:30 +0200

    update

---
+++ js/index.js
@@ -0,0 +1,21 @@
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+(function() {
+   var skjellsord = getParameterByName("skjellsord")
+   var button = document.getElementById("button");
+   button.innerHTML = skjellsord;
+})();
+
+
+function changeDisplay() {
+    document.getElementById("answer").style.display="block";
+    document.getElementById("question").style.display="none";
+}
