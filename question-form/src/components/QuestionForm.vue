86fd32284b1aa5d342ce2684dcfc974a4b8007a8
kristianoftedal <kristian.oftedal@pointtaken.no>
2018-02-28 17:57:00 +0100

    added firebase

--- question-form/src/components/QuestionForm.vue
+++ question-form/src/components/QuestionForm.vue
@@ -1,114 +1,114 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="eight columns">
+  <div class='container'>
+    <div class='row'>
+      <div class='eight columns'>
         <h2>Legg inn spørsmål</h2>
-        <form v-on:submit="addQuestion">
-          <div class="row">
-              <div class="twelve columns">
-                <label for="qtxt">Spørsmål:</label>
-                <input v-model="question.questionText" class="u-full-width" type="text" id="qtxt">
+        <form v-on:submit='addQuestion'>
+          <div class='row'>
+              <div class='twelve columns'>
+                <label for='qtxt'>Spørsmål:</label>
+                <input v-model='question.questionText' class='u-full-width' type='text' id='qtxt'>
               </div>
           </div>
-          <div class="row">
-            <div class="twelve columns">
-              <label for="category">Kategori</label>
-              <select class="u-full-width" id="category" v-model="question.category">
-                <option value="Ernæring og helse">Ernæring og helse</option>
-                <option value="Økologi og bærekraftig utvikling">Økologi og bærekraftig utvikling</option>
-                <option value="Bærekraftig utvikling">Bærekraftig utvikling</option>
-                <option value="Elektromagnetisk stråling">Elektromagnetisk stråling</option>
-                <option value="Radioaktiv stråling">Radioaktiv stråling</option>
-                <option value="Redoksreaksjoner">Redoksreaksjoner</option>
-                <option value="Energi for fremtiden">Energi for fremtiden</option>
-                <option value="DNA, proteinsyntesse og arv">DNA, proteinsyntesse og arv</option>
-                <option value="Bioteknologi">Bioteknologi</option>
+          <div class='row'>
+            <div class='twelve columns'>
+              <label for='category'>Kategori</label>
+              <select class='u-full-width' id='category' v-model='question.category'>
+                <option value='Ernæring og helse'>Ernæring og helse</option>
+                <option value='Økologi og bærekraftig utvikling'>Økologi og bærekraftig utvikling</option>
+                <option value='Bærekraftig utvikling'>Bærekraftig utvikling</option>
+                <option value='Elektromagnetisk stråling'>Elektromagnetisk stråling</option>
+                <option value='Radioaktiv stråling'>Radioaktiv stråling</option>
+                <option value='Redoksreaksjoner'>Redoksreaksjoner</option>
+                <option value='Energi for fremtiden'>Energi for fremtiden</option>
+                <option value='DNA, proteinsyntesse og arv'>DNA, proteinsyntesse og arv</option>
+                <option value='Bioteknologi'>Bioteknologi</option>
               </select>
             </div>
           </div>
-          <div class="row">
-            <div class="twelve columns">
-              <label for="bilde">Bilde?</label>
-              <select class="u-full-width" id="bilde" v-model="question.imageId">
-                <option value="">Nei</option>
-                <option v-bind:key="image.id" v-for="image in images" v-bind:value="image.id">{{ image.image }}</option>
+          <div class='row'>
+            <div class='twelve columns'>
+              <label for='bilde'>Bilde?</label>
+              <select class='u-full-width' id='bilde' v-model='question.imageId'>
+                <option value=''>Nei</option>
+                <option v-bind:key='image.id' v-for='image in images' v-bind:value='image.id'>{{ image.image }}</option>
               </select>
             </div>
           </div>
-          <div class="row">
-            <div class="six columns">
-              <label for="solution1">A:</label>
+          <div class='row'>
+            <div class='six columns'>
+              <label for='solution1'>A:</label>
               <input
-                class="u-full-width"
-                type="text"
-                v-model="question.answers[0].value"
-                id="solution1">
+                class='u-full-width'
+                type='text'
+                v-model='question.answers[0].value'
+                id='solution1'>
             </div>
           </div>
-          <div class="row">
-            <div class="six columns">
-              <label for="solution2">B:</label>
-              <input class="u-full-width"
-                v-model="question.answers[1].value"
-                type="text" id="solution2">
+          <div class='row'>
+            <div class='six columns'>
+              <label for='solution2'>B:</label>
+              <input class='u-full-width'
+                v-model='question.answers[1].value'
+                type='text' id='solution2'>
             </div>
           </div>
-          <div class="row">
-            <div class="six columns">
-              <label for="solution3">C:</label>
-              <input class="u-full-width"
-                v-model="question.answers[2].value"
-                type="text" id="solution3">
+          <div class='row'>
+            <div class='six columns'>
+              <label for='solution3'>C:</label>
+              <input class='u-full-width'
+                v-model='question.answers[2].value'
+                type='text' id='solution3'>
             </div>
           </div>
-          <div class="row">
-            <div class="six columns">
-              <label for="solution4">D:</label>
-              <input class="u-full-width"
-                v-model="question.answers[3].value"
-                type="text" placeholder="" id="solution4">
+          <div class='row'>
+            <div class='six columns'>
+              <label for='solution4'>D:</label>
+              <input class='u-full-width'
+                v-model='question.answers[3].value'
+                type='text' placeholder='' id='solution4'>
             </div>
           </div>
-          <div class="row">
-            <div class="six columns">
-              <label for="solution">FASIT:</label>
-              <select class="u-full-width" id="category" v-model="question.solution">
-                <option value="a">A</option>
-                <option value="b">B</option>
-                <option value="c">C</option>
-                <option value="d">D</option>
+          <div class='row'>
+            <div class='six columns'>
+              <label for='solution'>FASIT:</label>
+              <select class='u-full-width' id='category' v-model='question.solution'>
+                <option value='a'>A</option>
+                <option value='b'>B</option>
+                <option value='c'>C</option>
+                <option value='d'>D</option>
               </select>
             </div>
           </div>
-          <div class="row">
-            <div class="two columns">
-              <label for="difficulty">Vanskelighetsgrad</label>
-              <select class="u-full-width"
-                id="solution" v-model="question.difficulty">
-                <option value="Lett">Lett</option>
-                <option value="Middels">Middels</option>
-                <option value="Vanskelig">Vanskelig</option>
+          <div class='row'>
+            <div class='two columns'>
+              <label for='difficulty'>Vanskelighetsgrad</label>
+              <select class='u-full-width'
+                id='solution' v-model='question.difficulty'>
+                <option value='Lett'>Lett</option>
+                <option value='Middels'>Middels</option>
+                <option value='Vanskelig'>Vanskelig</option>
               </select>
             </div>
           </div>
-          <div class="row">
-            <div class="two columns">
-              <label for="lang">Språk</label>
-              <select class="u-full-width" id="lang" v-model="question.lang">
-                <option value="no">no</option>
-                <option value="en">en</option>
+          <div class='row'>
+            <div class='two columns'>
+              <label for='lang'>Språk</label>
+              <select class='u-full-width' id='lang' v-model='question.lang'>
+                <option value='no'>no</option>
+                <option value='en'>en</option>
               </select>
             </div>
           </div>
-          <input class="button-primary" type="submit" value="Submit">
-          <a v-on:click="downloadFile" class="button" type="button">Last ned</a>
+          <input class='button-primary' type='submit' value='Submit'>
+          <a v-on:click='downloadFile' class='button' type='button'>Last ned</a>
         </form>
       </div>
-      <div class="four columns">
+      <div class='four columns'>
         <ul>
-          <li v-bind:key="question.id" v-for="question in savedQuestions">
-            <a v-on:click="editQuestion(question.id)">{{question.questionText}}</a>
-            <button v-on:click="deleteQuestion(question.id)">X</button>
+          <li v-bind:key='question.id' v-for='question in questions'>
+            <a v-on:click='editQuestion(question)'>{{question.questionText}}</a>
+            <button v-on:click='deleteQuestion(question)'>X</button>
           </li>
         </ul>
       </div>
@@ -118,36 +118,39 @@
 
 <script>
 import uuid from 'uuid/v1';
-import QuestionsService from './questionsService';
 import images from './images';
+import firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyBCSbMVM4PbAKKDDqAs5Sx0reEgqLh8Q9Y',
+  authDomain: 'kjema-fda24.firebaseapp.com',
+  databaseURL: 'https://kjema-fda24.firebaseio.com',
+  projectId: 'kjema-fda24',
+  storageBucket: 'kjema-fda24.appspot.com',
+  messagingSenderId: '730985198123'
+};
+
+const app = firebase.initializeApp(config);
+const db = app.database();
+const naturfagQuestionsRef = db.ref('naturfagQuestions');
 
 export default {
   name: 'QuestionForm',
-  computed: {
-    savedQuestions() {
-      const questions = JSON.parse(localStorage.getItem('naturfagSpørsmål'));
-      return questions;
-    },
+  firebase: {
+    questions: naturfagQuestionsRef,
   },
   methods: {
     addQuestion() {
-      const questionsService = new QuestionsService();
-      questionsService.addQuestion(this.question);
+      naturfagQuestionsRef.push(this.question);
+      debugger;
     },
-    editQuestion(id) {
-      const question = this.savedQuestions.find(e => e.id === id);
-      const index = this.savedQuestions.findIndex(e => e.id === id);
-      const questionsService = new QuestionsService();
-      questionsService.removeQuestion(question.id);
-      this.savedQuestions.splice(index, 1);
+    editQuestion(question) {
       this.question = question;
+      this.deleteQuestion(question);
+      delete this.question['.key'];
     },
-    deleteQuestion(id) {
-      const questionsService = new QuestionsService();
-      questionsService.removeQuestion(id);
-      const index = this.savedQuestions.findIndex(e => e.id === id);
-      this.savedQuestions.splice(index, 1);
-      window.location.reload();
+    deleteQuestion(question) {
+      naturfagQuestionsRef.child(question['.key']).remove();
     },
     downloadFile() {
       const questions = JSON.parse(localStorage.getItem('naturfagSpørsmål'));
@@ -168,7 +171,7 @@ export default {
       question: {
         id: uuid(),
         questionText: '',
-        imageId: '8e846ed7-a6d7-443a-86aa-b1a4cd69b2c8',
+        imageId: '',
         answers: [
           {
             key: 'a',
@@ -190,7 +193,7 @@ export default {
         difficulty: 'Lett',
         solution: '',
         lang: 'no',
-        category: 'Ernæring og helse',
+        category: '',
       },
     };
   },
--- question-form/src/main.js
+++ question-form/src/main.js
@@ -2,9 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+import VueFire from 'vuefire';
 import App from './App';
 
 Vue.use(Vuetify);
+Vue.use(VueFire);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
