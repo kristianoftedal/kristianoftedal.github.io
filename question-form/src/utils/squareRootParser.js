960dcebeb5333f54c9c96012cc26d441623fb05d
kristianoftedal <kristian.oftedal@pointtaken.no>
2018-11-08 20:24:58 +0100

    fix square root parser

--- question-form/src/utils/squareRootParser.js
+++ question-form/src/utils/squareRootParser.js
@@ -5,15 +5,24 @@ const squareRootParser = (question) => {
   if (question == null) return (<Text />);
   const parts = question.split('$');
   const restructuredText = [];
+  let postSquareRoot = -1;
   for (let i = 0; i < parts.length; i += 1) {
-    if (parts[i] === '√') {
-      restructuredText.push(`<span class="squareRoot">${parts[i]}</span>`);
+    if (parts[i].indexOf('√') > -1) {
+      const tempParts = parts[i].split('√');
+      if (tempParts[0] === '') {
+        restructuredText.push('<span class="squareRoot">√</span>');
+        restructuredText.push(`<span class="squareRoot">${tempParts[1]}</span>`);
+      } else {
+        restructuredText.push(`<span class="squareRoot">${tempParts[0]}</span>`);
+        restructuredText.push('<span class="squareRoot">√</span>');
+      }
+      postSquareRoot = i + 2;
     } else if (parts[i] === '') {
       // do nothing
-    } else if (i < 2) {
-      restructuredText.push(`<span class="postSquareRoot"><span>${formulaParser(parts[i])}</span></span>`);
+    } else if (i < postSquareRoot) {
+      restructuredText.push(`<span class="postSquareRoot">${formulaParser(parts[i])}</span>`);
     } else {
-      restructuredText.push(`<span>${formulaParser(parts[i])}</span>`);
+      restructuredText.push(`<span class="">${formulaParser(parts[i])}</span>`);
     }
   }
   return restructuredText.join('');
