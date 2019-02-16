324f2b3eb20f1fbb37178234763108154a911221
kristianoftedal <kristian.oftedal@pointtaken.no>
2019-02-16 11:26:14 +0100

    fix

--- arcade/sw.js
+++ arcade/sw.js
@@ -10,16 +10,16 @@ workbox.precaching.precacheAndRoute([
     "revision": "aa98cd993bfd2c681b334d2e3a742ae2"
   },
   {
-    "url": "/_nuxt/3e42d3fd7e5f9889bcce.js",
-    "revision": "b63dfcf47a3c7cd6361a58af5ed78833"
-  },
-  {
-    "url": "/_nuxt/5b6f40b0188ac3c2c266.js",
-    "revision": "10a8cd6167a9e931645d6dd965c3c35f"
+    "url": "/_nuxt/67aa52b71a4df2b89095.js",
+    "revision": "f0d09ab401c18586b9b0f84ca3443450"
   },
   {
     "url": "/_nuxt/d6cd39794a1bfaad7b4c.js",
     "revision": "1151c921391a85091bcd12d1c23f9ce5"
+  },
+  {
+    "url": "/_nuxt/e2ea79e6f86a277cb06a.js",
+    "revision": "0c84797b1005114babf949704556c991"
   },
   {
     "url": "/_nuxt/fa9cb1d67a2db542936b.js",
