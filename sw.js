68730e423925975579dc8bda3b90c9b148a56d71
kristianoftedal <kristian.oftedal@pointtaken.no>
2019-03-08 08:07:36 +0100

    changes

--- sw.js
+++ sw.js
@@ -2,40 +2,40 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/16d7d6a40775e5718d16.js",
-    "revision": "d74e2d98ad235d9d950cae0bf868949e"
+    "url": "/_nuxt/325037545cf03638e7fe.js",
+    "revision": "48f5d6a1166a0d0e75c69583a2fcacea"
   },
   {
-    "url": "/_nuxt/23a457a6026e494f2dff.js",
-    "revision": "8291e7c5522a766ef9f43061efe49e61"
+    "url": "/_nuxt/3704d02d2d83edde6dae.js",
+    "revision": "148e6d3f117b594f8b5e3c306dc2e08f"
   },
   {
-    "url": "/_nuxt/a09bcda27f0d31ff9b25.js",
-    "revision": "d1c58fab62029ec0429d05a2040aa510"
+    "url": "/_nuxt/a2efa80484506c3754f6.js",
+    "revision": "00005a4b3bfb1db723ae47460f5c768e"
   },
   {
     "url": "/_nuxt/abd0834f5be32f482ec9.js",
     "revision": "2033f9e89805a04c2e87fefbe45446ee"
   },
   {
-    "url": "/_nuxt/bd9437a832e3a8d090b8.js",
-    "revision": "ac90b109f1b182f724424ef5f5d44edd"
+    "url": "/_nuxt/c0b58a0a7ea7292d916d.js",
+    "revision": "cb1ab6fe765d68967cf63022e6b55e1c"
   },
   {
     "url": "/_nuxt/c0fee2d932d53509cef1.js",
     "revision": "ad5a8ea9fec96d36b81ed29497162547"
   },
   {
-    "url": "/_nuxt/d1125085702dbec96320.js",
-    "revision": "9747a6154ca56762d4ec3d6e22e57c7e"
+    "url": "/_nuxt/da976b13646024c3e52d.js",
+    "revision": "a751d032d30a356d6972a152a7254119"
   },
   {
-    "url": "/_nuxt/ee0e97526f817f46c0fb.js",
-    "revision": "92047a5dfeee56bb46a19d8f264e3029"
+    "url": "/_nuxt/db5d118654acc161e87d.js",
+    "revision": "eaabf112fe67dd01adfef27e320ee7eb"
   },
   {
-    "url": "/_nuxt/fbada9ffb8960020d424.js",
-    "revision": "75dfb6fa918776606e44c1955d918c2d"
+    "url": "/_nuxt/f6956256a2b485139482.js",
+    "revision": "543bde2de717e3f2ca20ce5c46395a21"
   }
 ], {
   "cacheId": "kjemia-web",
